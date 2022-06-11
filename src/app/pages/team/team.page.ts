c61197a9a6b6612d56459f6b0df814480f5355e4
Gabriel De La Hoz <ga.de.di@hotmail.com>
2022-06-11 12:23:17 -0500

    Colors, team player list and segment

--- src/app/pages/team/team.page.ts
+++ src/app/pages/team/team.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-team',
   templateUrl: 'team.page.html',
   styleUrls: ['team.page.scss']
 })
-export class teamPage {
+export class teamPage implements OnInit{
+  default = "players";
+
+  @ViewChild(IonSegment) segment: IonSegment;
 
   constructor() {}
+
+  ngOnInit(){
+    //this.segment.value = "players";
+  }
+
+  segmentChanged( event ){
+
+    this.default = event.detail.value;
+
+    /*const valorSegment = event.detail.value;
+    console.log(valorSegment);*/
+
+  }
 
 }
