f5e8f0cb28ddbb7a09db572536855b7d77c6d8b5
Gabriel De La Hoz <ga.de.di@hotmail.com>
2022-08-05 01:45:10 -0500

    Login form validation and team-update

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserI } from 'src/app/models/user';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -17,67 +18,85 @@ export class LoginPage implements OnInit {
   user: UserI;
   team: Team;
   player: Player;
-
-  usuario = {
-    email: null,
-    password: null
-  }
+  loginForm: FormGroup;
+  isSubmitted = false;
 
   constructor(
     private router: Router,
     private auth: AuthService,
     private interaction: InteractionService,
-    private firestoreService: FirestoreService
+    private firestoreService: FirestoreService,
+    public formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]],
+      password: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(6)]]
+    })
+  }
+
+  showPassword(input: any, icon: any): any {
+    input.type = input.type === 'password' ?  'text' : 'password';
+    icon.name = icon.name === 'eye' ? 'eye-off' : 'eye';
+  }
+
+  get errorControl() {
+    return this.loginForm.controls;
   }
 
   //Función asincrona de login, recibe la promesa de firbase implementada en el servicio de Auth
-  async login(form){
+  async login(){
     await this.interaction.showLoading('Ingresando...');
-    const res = await this.auth.login(this.usuario.email, this.usuario.password).catch(error => {
+    this.isSubmitted = true;
+    if (!this.loginForm.valid) {
       this.interaction.closeLoading();
-      this.interaction.presentToast('Usuario o contraseña invalido.');
-    });
-
-    if (res) {
-      //console.log('res -> ', res);
-
-      await new Promise((resolve) => {
-        this.firestoreService.getUserData(res.user.uid).subscribe(async res => {
-          this.user = await res;
-          //console.log(this.user);
-          localStorage.setItem('infoUser', JSON.stringify(this.user));
-          resolve("Promise result");
-        });
+      this.interaction.presentToast('Porfavor escriba todos los campos correctamente.');
+      return false;
+    } else {
+      const res = await this.auth.login(this.loginForm.value.email, this.loginForm.value.password).catch(error => {
+        this.interaction.closeLoading();
+        this.interaction.presentToast('Usuario o contraseña invalido.');
       });
 
-      //console.log(this.user.teamId);
-      
-      await new Promise((resolve) => {
-        this.firestoreService.getTeamData(this.user.teamId).subscribe(async res => {
-          this.team = await res;
-          //console.log(this.team);
-          localStorage.setItem('infoTeam', JSON.stringify(this.team));
-          resolve("Promise result");
-        });
-      })
-      
-      if (this.user.rol == 'Jugador') {
+      if (res) {
+        //console.log('res -> ', res);
+
         await new Promise((resolve) => {
-          this.firestoreService.getPlayerData(this.user.uid).subscribe(async res => {
-            this.player = await res;
-            //console.log(this.player);
-            localStorage.setItem('infoPlayer', JSON.stringify(this.player));
+          this.firestoreService.getUserData(res.user.uid).subscribe(async res => {
+            this.user = await res;
+            //console.log(this.user);
+            localStorage.setItem('infoUser', JSON.stringify(this.user));
+            resolve("Promise result");
+          });
+        });
+
+        //console.log(this.user.teamId);
+        
+        await new Promise((resolve) => {
+          this.firestoreService.getTeamData(this.user.teamId).subscribe(async res => {
+            this.team = await res;
+            //console.log(this.team);
+            localStorage.setItem('infoTeam', JSON.stringify(this.team));
             resolve("Promise result");
           });
         })
-      }
+        
+        if (this.user.rol == 'Jugador') {
+          await new Promise((resolve) => {
+            this.firestoreService.getPlayerData(this.user.uid).subscribe(async res => {
+              this.player = await res;
+              //console.log(this.player);
+              localStorage.setItem('infoPlayer', JSON.stringify(this.player));
+              resolve("Promise result");
+            });
+          })
+        }
 
-      this.interaction.closeLoading();
-      this.interaction.presentToast('Usuario ingresado con exito.');
-      this.router.navigate(['/tabs/home']);
+        this.interaction.closeLoading();
+        this.interaction.presentToast('Usuario ingresado con exito.');
+        this.router.navigate(['/tabs/home']);
+      }
     }
   }
 
--- src/app/pages/team-update/team-update.page.ts
+++ src/app/pages/team-update/team-update.page.ts
@@ -6,6 +6,7 @@ import { Team } from 'src/app/models/team';
 import { InteractionService } from 'src/app/services/interaction.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-team-update',
@@ -16,18 +17,47 @@ export class TeamUpdatePage implements OnInit {
   teamData: Team;
   team: Team;
   file: any;
-  imgState: boolean = false;
+  teamForm: FormGroup;
+  logoForm: FormGroup;
+  bannerForm: FormGroup;
+  isSubmitted = false;
+  maxDate = new Date().toISOString().split("T")[0];
 
   constructor(
-    private router: Router,
     private firestoreService: FirestoreService,
     private interaction: InteractionService,
     private angularStorage: AngularFireStorage,
-    private nav: NavController
+    private nav: NavController,
+    public formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
     this.getTeamData();
+    this.teamForm = this.formBuilder.group({
+      name: [this.teamData.name, [Validators.required, Validators.minLength(4), Validators.maxLength(40)]],
+      country: [this.teamData.country, [Validators.required]],
+      campus: [this.teamData.campus, [Validators.required]],
+      birthday: [this.teamData.birthday, [Validators.required]],
+      description: [this.teamData.description, [Validators.maxLength(200)]],
+    })
+    this.logoForm = this.formBuilder.group({
+      imgLogo: ['', [Validators.required]]
+    })
+    this.bannerForm = this.formBuilder.group({
+      imgBanner: ['', [Validators.required]]
+    })
+  }
+
+  get errorControlTeam() {
+    return this.teamForm.controls;
+  }
+
+  get errorControlLogo() {
+    return this.logoForm.controls;
+  }
+
+  get errorControlBanner() {
+    return this.bannerForm.controls;
   }
 
   getTeamData(){
@@ -68,79 +98,94 @@ export class TeamUpdatePage implements OnInit {
 
   async updateImgBannerTeam(){
     await this.interaction.showLoading('Actualizando imagen...');
-    let img;
-    await this.enviar().then(res => { img = res })
-    console.log('RUTA IMAGEN ->>', img);
-    
-    this.firestoreService.editTeam(this.teamData.id, {imgBanner: img});
-
-    await new Promise((resolve) => {
-      this.firestoreService.getTeamData(this.teamData.id).subscribe(async res => {
-        this.team = await res;
-        //console.log(this.team);
-        localStorage.setItem('infoTeam', JSON.stringify(this.team));
-        resolve("Promesa resuelta");
+    this.isSubmitted = true;
+    if (!this.bannerForm.valid) {
+      this.interaction.closeLoading();
+      return false;
+    } else {
+      let img;
+      await this.enviar().then(res => { img = res })
+      console.log('RUTA IMAGEN ->>', img);
+      
+      this.firestoreService.editTeam(this.teamData.id, {imgBanner: img});
+  
+      await new Promise((resolve) => {
+        this.firestoreService.getTeamData(this.teamData.id).subscribe(async res => {
+          this.team = await res;
+          //console.log(this.team);
+          localStorage.setItem('infoTeam', JSON.stringify(this.team));
+          resolve("Promesa resuelta");
+        });
       });
-    });
-    
-    this.interaction.closeLoading();
-    this.interaction.presentToast('Imagen actualizada.');
-    this.nav.navigateBack(['/tabs/home']);
+      
+      this.interaction.closeLoading();
+      this.interaction.presentToast('Imagen actualizada.');
+      this.nav.navigateBack(['/tabs/home']);
+    }
   }
 
   async updateImgLogoTeam(){
     await this.interaction.showLoading('Actualizando imagen...');
-    let img;
-    await this.enviar().then(res => { img = res })
-    console.log('RUTA IMAGEN ->>', img);
-    
-    this.firestoreService.editTeam(this.teamData.id, {imgLogo: img});
-
-    await new Promise((resolve) => {
-      this.firestoreService.getTeamData(this.teamData.id).subscribe(async res => {
-        this.team = await res;
-        //console.log(this.team);
-        localStorage.setItem('infoTeam', JSON.stringify(this.team));
-        resolve("Promesa resuelta");
+    this.isSubmitted = true;
+    if (!this.logoForm.valid) {
+      this.interaction.closeLoading();
+      return false;
+    } else {
+      let img;
+      await this.enviar().then(res => { img = res })
+      console.log('RUTA IMAGEN ->>', img);
+      
+      this.firestoreService.editTeam(this.teamData.id, {imgLogo: img});
+  
+      await new Promise((resolve) => {
+        this.firestoreService.getTeamData(this.teamData.id).subscribe(async res => {
+          this.team = await res;
+          //console.log(this.team);
+          localStorage.setItem('infoTeam', JSON.stringify(this.team));
+          resolve("Promesa resuelta");
+        });
       });
-    });
-    
-    this.interaction.closeLoading();
-    this.interaction.presentToast('Imagen actualizada.');
-    this.nav.navigateBack(['/tabs/home']);
+      
+      this.interaction.closeLoading();
+      this.interaction.presentToast('Imagen actualizada.');
+      this.nav.navigateBack(['/tabs/home']);
+    }
   }
 
-  async updateTeam(name, description, country, campus, birthday){
+  async updateTeam(){
     await this.interaction.showLoading('Actualizando...');
-    const datosTeam: Team = {
-      name: name.value,
-      description: description.value,
-      country: country.value,
-      campus: campus.value,
-      birthday: birthday.value
-    }
-    this.firestoreService.editTeam(this.teamData.id, datosTeam);
+    this.isSubmitted = true;
 
-    await new Promise((resolve) => {
-      this.firestoreService.getTeamData(this.teamData.id).subscribe(async res => {
-        this.team = await res;
-        //console.log(this.team);
-        localStorage.setItem('infoTeam', JSON.stringify(this.team));
-        resolve("Promesa resuelta");
+    if (!this.teamForm.valid) {
+      this.interaction.closeLoading();
+      return false;
+    } else {
+      const datosTeam: Team = {
+        name: this.teamForm.value.name,
+        description: this.teamForm.value.description,
+        country: this.teamForm.value.country,
+        campus: this.teamForm.value.campus,
+        birthday: this.teamForm.value.birthday
+      }
+      this.firestoreService.editTeam(this.teamData.id, datosTeam);
+  
+      await new Promise((resolve) => {
+        this.firestoreService.getTeamData(this.teamData.id).subscribe(async res => {
+          this.team = await res;
+          //console.log(this.team);
+          localStorage.setItem('infoTeam', JSON.stringify(this.team));
+          resolve("Promesa resuelta");
+        });
       });
-    });
-    
-    this.interaction.closeLoading();
-    this.interaction.presentToast('Datos de equipo actualizados correctamente.');
-    this.nav.navigateBack(['/tabs/home']);
+      
+      this.interaction.closeLoading();
+      this.interaction.presentToast('Datos de equipo actualizados correctamente.');
+      this.nav.navigateBack(['/tabs/home']);
+    }
   }
 
   uploadImg($event: any) {
     this.file = $event.target.files[0];
     //console.log(this.file);
-  }
-
-  onSelectImgUpdate(){
-    this.imgState = true;
   }
 }
