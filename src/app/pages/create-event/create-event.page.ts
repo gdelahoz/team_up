e519a5a7bf18aab53e13018cc0d3eceb4b2f42a9
Gabriel De La Hoz <ga.de.di@hotmail.com>
2022-08-05 13:53:47 -0500

    Create events validators

--- src/app/pages/create-event/create-event.page.ts
+++ src/app/pages/create-event/create-event.page.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { NavController } from '@ionic/angular';
@@ -20,17 +21,34 @@ export class CreateEventPage implements OnInit {
   team: Team;
   file: any;
   imgState: boolean = false;
+  eventForm: FormGroup;
+  isSubmitted = false;
+  minDate = new Date().toISOString().split("T")[0];
 
   constructor(
     private nav: NavController,
     private interaction: InteractionService, 
     private angularStorage: AngularFireStorage,
-    private eventService: EventService
+    private eventService: EventService,
+    public formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
     this.getUserData();
     this.getTeamData();
+    this.eventForm = this.formBuilder.group({
+      type: ['', Validators.required],
+      place: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(40)]],
+      imgPlace: [''],
+      startTime: ['', [Validators.required]],
+      endTime: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      info: ['', [Validators.required, Validators.maxLength(200)]]
+    })
+  }
+
+  get errorControl() {
+    return this.eventForm.controls;
   }
 
   getUserData(){
@@ -75,45 +93,50 @@ export class CreateEventPage implements OnInit {
 
   async createEvent(type, date, place, info, startTime, endTime){
     await this.interaction.showLoading('Publicando...');
-
-    let img;
-    if (this.file == null) {
-      img = '';
-    } else {
-      await this.enviar().then(res => { img = res });
-    }
-    
-    const datos: Event = {
-      teamId: this.teamData.id,
-
-      type: type.value,
-      date: date.value,
-      imgPlace: img,
-      place: place.value,
-      info: info.value,
-      startTime: startTime.value,
-      endTime: endTime.value,
-      attendance: [],
-      absence: [],
-
-      createdAt: Timestamp.now(),
-      updatedAt: Timestamp.now()
-    }
-
-    this.eventService.createEvents(datos).catch(error => {
+    this.isSubmitted = true;
+    if (!this.eventForm.valid) {
       this.interaction.closeLoading();
-      this.interaction.presentToast('Ocurrió un error al crear la actividad.');
-      console.log('Error anuncio ->', error);
-    })
-
-    await new Promise((resolve) => {
-      this.eventService.getEvents(this.teamData.id);
-      resolve("Promesa resuelta");
-    });
-    
-    this.interaction.closeLoading();
-    this.interaction.presentToast('Actividad creada correctamente.');
-    this.nav.navigateBack(['/tabs/calendar']);
+      return false;
+    } else {
+      let img;
+      if (this.file == null) {
+        img = '';
+      } else {
+        await this.enviar().then(res => { img = res });
+      }
+      
+      const datos: Event = {
+        teamId: this.teamData.id,
+  
+        type: type.value,
+        date: date.value,
+        imgPlace: img,
+        place: place.value,
+        info: info.value,
+        startTime: startTime.value,
+        endTime: endTime.value,
+        attendance: [],
+        absence: [],
+  
+        createdAt: Timestamp.now(),
+        updatedAt: Timestamp.now()
+      }
+  
+      this.eventService.createEvents(datos).catch(error => {
+        this.interaction.closeLoading();
+        this.interaction.presentToast('Ocurrió un error al crear la actividad.');
+        console.log('Error anuncio ->', error);
+      })
+  
+      await new Promise((resolve) => {
+        this.eventService.getEvents(this.teamData.id);
+        resolve("Promesa resuelta");
+      });
+      
+      this.interaction.closeLoading();
+      this.interaction.presentToast('Actividad creada correctamente.');
+      this.nav.navigateBack(['/tabs/calendar']);
+    }
   }
 
   uploadImg($event: any) {
